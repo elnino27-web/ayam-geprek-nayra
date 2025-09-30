6311141ff8876b0b1431b86feb54523aff077336
Washiatul Akmal <akmalwahiatul08@gmail.com>
2025-09-30 18:39:38 +0800

    Konek form reservasi ke wa

--- JS/script.js
+++ JS/script.js
@@ -16,3 +16,37 @@ document.addEventListener('click', function(e){
         navbarNav.classList.remove('active');
     }
 })
+
+// Konek form reservasi ke wa
+document.getElementById("reservationForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  var nama = document.getElementById("nama").value;
+  var nomor = document.getElementById("nomor").value;
+  var jumlah = document.getElementById("jumlah").value;
+  var meja = document.getElementById("meja").value;
+  var waktu = document.getElementById("waktu").value;
+  var tanggal = document.getElementById("tanggal").value;
+  var catatan = document.getElementById("catatan").value;
+  
+  if (!nama || !nomor || !jumlah || !meja || !waktu || !tanggal || !catatan) {
+    alert("Mohon isi semua data sebelum mengirim!");
+    return;
+  }
+
+  var text = "Halo Kedai Nayra, saya " + nama + " (" + nomor + ")\nIngin melakukan reservasi tempat di Keday Nayra, berikut keterangan saya lampirkan:\n\n" + 
+  "Jumlah Orang\t\t: " + jumlah + "\n" +
+  "Tempat Pilihan\t\t: " + meja + "\n" +
+  "Waktu Kedatangan\t: " + waktu + "\n" +
+  "Tanggal Kedatangan\t: " + tanggal + "\n" +
+  "Catatan Tambahan\t: " + catatan + "\n\n" +
+  "Terima Kasih!";
+  var url =
+    "https://api.whatsapp.com/send?phone=6281242337310&text=" +
+    encodeURIComponent(text);
+
+  alert("Reservasi melalui WhatsApp");
+
+  window.open(url, "_blank");
+});
+
